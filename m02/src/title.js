2c867b231ebe1e8262b0f525a3b16123687d69d1
Thadeu Cotts <thadeu@feracode.com>
2019-01-28 00:26:30 -0200

    lesson 23

--- m02/src/title.js
+++ m02/src/title.js
@@ -2,19 +2,25 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  getDefaultProps () {
-    return {
-      name: 'Desconhecido',
-      lastName: {
-        first: 'Sem ',
-        last: 'Sobrenome'
-      }
-    }
-  },
-  render () {
-    return <h1> Olá {`${this.props.name} ${this.props.lastName.first}${this.props.lastName.last}`}!</h1>
-  }
-})
+const Title = ({name, lastName}) => <h1> Olá {`${name} ${lastName}`}!</h1>
+
+Title.defaultProps = {
+  name: 'Desconhecido',
+      lastName: 'Sobrenome'
+}
+// const Title = React.createClass({
+//   getDefaultProps () {
+//     return {
+//       name: 'Desconhecido',
+//       lastName: {
+//         first: 'Sem ',
+//         last: 'Sobrenome'
+//       }
+//     }
+//   },
+//   render () {
+//     return <h1> Olá {`${this.props.name} ${this.props.lastName.first}${this.props.lastName.last}`}!</h1>
+//   }
+// })
 
 export default Title
