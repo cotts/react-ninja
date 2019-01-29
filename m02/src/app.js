06b6796005d02037b7255874e3f8f0647db2c98c
Thadeu Cotts <thadeu@feracode.com>
2019-01-28 23:51:05 -0200

    lesson 25

--- m02/src/app.js
+++ m02/src/app.js
@@ -1,13 +1,13 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
+import Square from './square'
 
 class App extends Component{
   render () {
     return (
       <div className='container'>
-        <Title name='Thadeu' />
+        {['blue', 'red' , 'green'].map((square) => <Square key={square} color={square} /> )}
       </div>
     )
   }
