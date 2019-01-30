c3d11535c042672e4062457d430147e4810291db
Thadeu Cotts <thadeu@feracode.com>
2019-01-29 23:00:48 -0200

    lesson 28

--- m1.2/src/app.js
+++ m1.2/src/app.js
@@ -2,12 +2,16 @@
 
 import React, { Component } from 'react'
 import Square from './square'
+import Button from './button'
 
 class App extends Component{
   render () {
     return (
       <div className='container' onClick={ e => alert('clicou')}>
-      <Square />
+      <Button>
+        Text 
+        <span> complete</span>
+      </Button>
       </div>
     )
   }
---
+++ m1.2/src/button.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+
+const Button = ({ children }) => (
+  <button>
+    { children }
+  </button>
+
+)
+
+export default Button
